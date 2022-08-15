5f204f86dc3f3fe05ffb11e06d80b134195bd251
Evan Smith <evan.leigh@outlook.com>
2022-08-15 01:51:09 -0400

    play or pause with react hooks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,63 +1,65 @@
-import anime from "animejs";
-import { useEffect } from "react";
+import React, { ChangeEvent, useState } from "react";
 
-const TimeElapsed = function () {
-  return (
-    <svg
-      width="213"
-      height="213"
-      viewBox="0 0 213 213"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="overflow-visible rotate-[-90deg]"
-    >
-      <path
-        d="M213 106.5C213 165.318 165.318 213 106.5 213C47.6817 213 0 165.318 0 106.5C0 47.6817 47.6817 0 106.5 0C165.318 0 213 47.6817 213 106.5Z"
-        stroke="#FFC107"
-        style={{ shapeRendering: "revert-layer" }}
-        strokeWidth="10"
-        id="time-elapsed"
-      />
-    </svg>
-  );
-};
+import { useInterval } from "usehooks-ts";
 
-const TimeDuration = function () {
-  return (
-    <svg
-      width="213"
-      height="213"
-      viewBox="0 0 213 213"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="overflow-visible rotate-[-90deg] fixed"
-    >
-      <path
-        d="M213 106.5C213 165.318 165.318 213 106.5 213C47.6817 213 0 165.318 0 106.5C0 47.6817 47.6817 0 106.5 0C165.318 0 213 47.6817 213 106.5Z"
-        stroke="#000000"
-        strokeWidth="10"
-        id="time-duration"
-      />
-    </svg>
-  );
-};
+import { TimeDuration } from "../components/timer";
 
 export default function Home() {
-  useEffect(() => {
-    let start = anime({
-      targets: "#time-elapsed",
-      strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 4000,
-      direction: "forward",
-      easing: "linear",
-      loop: true,
-    });
-    start.play();
-  });
+  const [count, setCount] = useState<number>(0);
+  const [isPlaying, setPlaying] = useState<boolean>(false);
+
+  useInterval(
+    () => {
+      setCount(count + 1);
+    },
+    isPlaying ? 1000 : null // Every second
+  );
+
+  function parseTime(time: number) {
+    let minutes: any = Math.floor(time / 60);
+    let seconds: any = time - minutes * 60;
+
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    return minutes + ":" + seconds;
+  }
+
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-[#1D1F21]">
+    <div className="w-full h-screen flex items-center justify-center bg-[#16181A]">
       <TimeDuration />
-      <TimeElapsed />
+      {/* <TimeElapsed ref={input} /> */}
+
+      <svg
+        width="613"
+        height="613"
+        viewBox="0 0 213 213"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="overflow-visible rotate-[-90deg] fixed pointer-events-none"
+      >
+        <path
+          d="M213 106.5C213 165.318 165.318 213 106.5 213C47.6817 213 0 165.318 0 106.5C0 47.6817 47.6817 0 106.5 0C165.318 0 213 47.6817 213 106.5Z"
+          stroke="#FFC107"
+          style={{ shapeRendering: "revert-layer" }}
+          strokeWidth="10"
+          // strokeLinecap="round"
+          id="time-elapsed"
+        />
+      </svg>
+
+      {/* <div ref={input}>hello</div> */}
+      <div className="text-white text-center">
+        <h1 className="font-mono text-4xl fixed left-1/2 translate-x-[-50%]">
+          {parseTime(count)}
+        </h1>
+        <button
+          onClick={() => setPlaying(!isPlaying)}
+          className="relative bottom-[-70px]"
+        >
+          {isPlaying ? "pause" : "play"}
+        </button>
+      </div>
     </div>
   );
 }
