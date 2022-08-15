0a4a323619b0555d9ab459a8966ab2fecc9381bb
Evan Smith <evan.leigh@outlook.com>
2022-08-15 03:27:43 -0400

    timer now updates with svg circle

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,24 @@ import { useInterval } from "usehooks-ts";
 
 import { TimeDuration } from "../components/timer";
 
+import {
+  CountdownCircleTimer,
+  useCountdown,
+} from "react-countdown-circle-timer";
+
 export default function Home() {
-  const [count, setCount] = useState<number>(0);
+  let duration = 30;
+  const [count, setCount] = useState<number>(duration);
   const [isPlaying, setPlaying] = useState<boolean>(false);
 
   useInterval(
     () => {
-      setCount(count + 1);
+      if (count === 0) {
+        setPlaying(false);
+      }
+      if (isPlaying) {
+        setCount(count - 1);
+      }
     },
     isPlaying ? 1000 : null // Every second
   );
@@ -27,31 +38,26 @@ export default function Home() {
 
   return (
     <div className="w-full h-screen flex items-center justify-center bg-[#16181A]">
-      <TimeDuration />
-      {/* <TimeElapsed ref={input} /> */}
+      <div className="fixed w-[500px] h-[500px] text-white">
+        <CountdownCircleTimer
+          isPlaying={isPlaying}
+          duration={duration}
+          size={500}
+          strokeWidth={20}
+          trailStrokeWidth={11}
+          colors={["#FFA500", "#FFA500", "#FFA500", "#A30000"]}
+          trailColor="#2b2f33"
+          colorsTime={[7, 5, 2, 0]}
+        >
+          {({ remainingTime }) => (
+            <h1 className="text-4xl font-mono">{parseTime(remainingTime)}</h1>
+          )}
+        </CountdownCircleTimer>
+      </div>
 
-      <svg
-        width="613"
-        height="613"
-        viewBox="0 0 213 213"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="overflow-visible rotate-[-90deg] fixed pointer-events-none"
-      >
-        <path
-          d="M213 106.5C213 165.318 165.318 213 106.5 213C47.6817 213 0 165.318 0 106.5C0 47.6817 47.6817 0 106.5 0C165.318 0 213 47.6817 213 106.5Z"
-          stroke="#FFC107"
-          style={{ shapeRendering: "revert-layer" }}
-          strokeWidth="10"
-          // strokeLinecap="round"
-          id="time-elapsed"
-        />
-      </svg>
-
-      {/* <div ref={input}>hello</div> */}
       <div className="text-white text-center">
         <h1 className="font-mono text-4xl fixed left-1/2 translate-x-[-50%]">
-          {parseTime(count)}
+          {}
         </h1>
         <button
           onClick={() => setPlaying(!isPlaying)}
