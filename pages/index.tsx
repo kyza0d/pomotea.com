c7fee4ec3bc7eecfab2cf984704e9bfaacbc6fd3
evan-leigh <evan.leigh@outlook.com>
2022-08-21 08:58:39 -0400

    simplifying code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,116 +1,101 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from 'react'
 
-import { useInterval } from "usehooks-ts";
+import { BsPlayFill, BsPauseFill } from 'react-icons/bs'
+import { FaUndoAlt, FaRegSun, FaSlidersH } from 'react-icons/fa'
+import { FiSettings, FiSliders } from 'react-icons/fi'
 
-import { TimeDuration } from "../components/timer";
+import moment from 'moment'
 
-import { BsPlayFill, BsPauseFill } from "react-icons/bs";
-import { FaUndoAlt, FaRegSun, FaSlidersH } from "react-icons/fa";
-import { FiSettings, FiSliders } from "react-icons/fi";
-
-import moment from "moment";
-
-import {
-  CountdownCircleTimer,
-  useCountdown,
-} from "react-countdown-circle-timer";
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 
 export default function Home() {
-  let duration: number = 30; // seconds
+  let [duration, setDuration] = useState(5)
 
-  let [windowWidth, setWindowWidth] = useState<number>(0);
-  let [windowHeight, setWindowHeight] = useState<number>(0);
-  let [circleSize, setCircleSize] = useState<number>(0);
+  let [count, setCount] = useState<number>(duration)
 
-  useEffect(() => {
-    setWindowWidth(window.innerWidth);
-    setWindowWidth(window.innerHeight);
-    setCircleSize((windowWidth + windowHeight) * 0.5);
+  const [isPlaying, setPlaying] = useState<boolean>(false)
 
-    window.addEventListener("resize", () => {
-      setWindowWidth(window.innerWidth);
-      setWindowHeight(window.innerHeight);
-      setCircleSize(windowHeight * 0.5);
-    });
-  });
+  const [key, setKey] = useState(0)
 
-  // Keep state for timer
-  const [count, setCount] = useState<number>(duration);
+  const [iteration, setIteration] = useState(0)
 
-  const [isPlaying, setPlaying] = useState<boolean>(false);
+  function Clock() {
+    const tasks = 8
 
-  const [key, setKey] = useState(0);
+    const work_time = 10
+    const break_time = 5
 
-  useInterval(
-    () => {
-      if (count <= 0) {
-        setPlaying(false); // alert("Time's up!");
+    const sessions = []
+
+    let working = true
+
+    working ? setCount(work_time) : setCount(break_time)
+
+    for (let i = 0; i < tasks; i++) {
+      sessions.push(i)
+
+      sessions[i] = {
+        type: working ? 'working' : 'break', // working or break
+        duration: working ? work_time : break_time, // duration in seconds
       }
-      if (isPlaying) {
-        setCount(count - 0.01);
-      }
-    },
-    isPlaying ? 10 : null
-  );
+
+      working = !working
+    }
+
+    if (sessions.length % 2 === 0) {
+      sessions.pop()
+    }
+
+    return <>{count}</>
+  }
+  const children = ({ remainingTime }: { remainingTime: any }) => {
+    count = remainingTime // use react-countdown-circle-timer's internal timer
+    return <Clock />
+  }
 
   return (
-    <div className="bg-[#101416] text-[#d6d6d6] grid place-items-center h-screen">
-      <div className="grid flex place-items-center gap-10">
-        <div>
-          <CountdownCircleTimer
-            isPlaying={isPlaying}
-            duration={duration}
-            key={key}
-            size={circleSize}
-            strokeWidth={windowWidth * 0.025}
-            trailStrokeWidth={windowWidth * 0.025}
-            colorsTime={[0, 0, 9, 0]}
-            colors={["#00ACCE", "#00ACCE", "#00ACCE", "#00ACCE"]}
-            trailColor="#202326"
-          >
-            {() => (
-              <h1 className="text-4xl font-mono fixed top-[41vh]">
-                {moment().minute(0).second(count).format("mm:ss")}
-              </h1>
-            )}
-          </CountdownCircleTimer>
-        </div>
+    <main>
+      <CountdownCircleTimer
+        isPlaying={isPlaying}
+        // children={children}
+        // colors='url(#fill)'
+        colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+        colorsTime={[7, 5, 2, 0]}
+        children={children}
+        duration={duration}
+        strokeWidth={20}
+        trailStrokeWidth={10}
+        strokeLinecap='round'
+        key={key}
+        size={500}
+        trailColor='#1b1e21'
+        onComplete={() => {
+          setIteration(iteration + 1)
+          console.log(iteration)
+          // do your stuff here
+          return { shouldRepeat: true, delay: 0 } // repeat animation in 1.5 seconds
+        }}
+      />
+      <div>
+        <button
+          onClick={() => {
+            setPlaying(false)
+            setCount(duration)
+            setKey((prevKey) => prevKey + 1)
+          }}>
+          <FaUndoAlt />
+        </button>
 
-        <div className="flex items-center gap-6">
-          <button
-            onClick={() => {
-              setPlaying(false);
-              setCount(duration);
-              setKey((prevKey) => prevKey + 1);
-            }}
-          >
-            <div className="rounded-full p-4 text-[#aaaaaa]">
-              <FaUndoAlt className="text-[3vw]" />
-            </div>
-          </button>
-
-          <button
-            onClick={() => {
-              setPlaying(!isPlaying);
-              console.log(count);
-            }}
-          >
-            <div className="rounded-full p-6 bg-[#00ACCE] text-white ">
-              {isPlaying ? (
-                <BsPauseFill className="text-[5vw]" />
-              ) : (
-                <BsPlayFill className="text-[5vw]" />
-              )}
-            </div>
-          </button>
-
-          <button>
-            <div className="rounded-full p-4 border-1 border-[#666666] text-[#aaaaaa]">
-              <FiSettings className="text-[3vw]" />
-            </div>
-          </button>
-        </div>
+        <button
+          onClick={() => {
+            setPlaying(!isPlaying)
+          }}>
+          <div>{isPlaying ? <BsPauseFill /> : <BsPlayFill />}</div>
+        </button>
+        <button>
+          <FiSettings />
+        </button>
       </div>
-    </div>
-  );
+    </main>
+  )
 }
