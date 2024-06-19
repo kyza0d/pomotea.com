65d54928021c456335f0ce669ccd8debcf219c44
Evan Smith <evan.leigh@outlook.com>
2024-06-19 08:02:03 -0400

    fix: Remove unnessesary files/code

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,17 +23,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="overflow-x-hidden">
         <SettingsProvider>
           <Toaster />
-
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Theme>
-              {children}
-            </Theme>
-          </ThemeProvider>
+          <Theme>
+            {children}
+          </Theme>
         </SettingsProvider>
       </body>
     </html >
